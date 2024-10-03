a3634d06ec32b94975c5a5840c6a00fb0203ee7b
joaonunez <joaovaldiglesias@gmail.com>
2024-10-03 10:42:42 -0300

    changued url database

--- app.py
+++ app.py
@@ -16,8 +16,9 @@ app = Flask(__name__)
 # Configuración de base de datos (usando variable de entorno para mayor seguridad)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv(
     "DATABASE_URL", 
-    "mysql://root:kYkDChFJJaDcDvfMISLvVrnJzyDdFcPw@junction.proxy.rlwy.net:26699/railway"
+    "mysql+pymysql://root:kYkDChFJJaDcDvfMISLvVrnJzyDdFcPw@junction.proxy.rlwy.net:26699/railway"
 )
+
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
