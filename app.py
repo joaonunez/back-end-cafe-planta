5d83a5eaa177e33f3bf481b544bf6b6cd851be7c
joaonunez <joaovaldiglesias@gmail.com>
2024-12-11 16:57:39 -0300

    production

--- app.py
+++ app.py
@@ -132,4 +132,4 @@ def create_app(config_name="default"):
 # ------------------------------------
 if __name__ == "__main__": 
     app = create_app()
-    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 3001)), debug=False)
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 3001)))
